c4cfedefcc68c2f4b8613b35bc7d95d0c3f322d0
Tobiillz <ayoadeopeoluwa@yahoo.com>
2021-07-30 10:04:04 +0100

    Add files

---
+++ main.js
@@ -0,0 +1,13 @@
+let inputBtn = document.querySelector('.input-btn')
+inputBtn.addEventListener('click',function(){
+    console.log('btn clicked')
+})
+
+let tabBtn = document.querySelector('.tab-btn')
+tabBtn.addEventListener('click',function(){
+    console.log('tab clicked')
+})
+let deleteBtn = document.querySelector('#delete-btn')
+deleteBtn.addEventListener('click',function(){
+    console.log('delete clicked')
+})
