ca57a546c49fe4f720b4866b4d82d395bda7bf22
Tobiillz <ayoadeopeoluwa@yahoo.com>
2021-07-31 23:06:50 +0100

    Some refactoring

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-let inputBtn = document.querySelector('.input-btn')
+let myLeads = ["Opeoluwa","Emeka","Tunji","Dele"]
+const inputEl = document.getElementById('input-el')
+const inputBtn = document.querySelector('.input-btn')
+const ulEl = document.getElementById('list-el')
+
 inputBtn.addEventListener('click',function(){
-    console.log('btn clicked')
+    myLeads.push(inputEl.value)
+    // console.log(myLeads)
+    for (let i=0; i<myLeads.length; i++){
+        console.log(myLeads[i])
+        ulEl.textContent = myLeads[i]
+    }
 })
 
 let tabBtn = document.querySelector('.tab-btn')
 tabBtn.addEventListener('click',function(){
-    console.log('tab clicked')
+    console.log(inputEl.value)
 })
 let deleteBtn = document.querySelector('#delete-btn')
 deleteBtn.addEventListener('click',function(){
