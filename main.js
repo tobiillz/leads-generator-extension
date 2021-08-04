a3d617f834cac2acecf36a67888efa2444161e52
Tobiillz <ayoadeopeoluwa@yahoo.com>
2021-08-04 23:53:53 +0100

    backtrack

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-let myLeads = []
+let myLeads = ["opeoluwa","david","emmanuel"]
 const inputEl = document.getElementById('input-el')
 const inputBtn = document.querySelector('.input-btn')
 const ulEl = document.getElementById('list-el')
 
 inputBtn.addEventListener('click',function(){
     myLeads.push(inputEl.value)
-    // console.log(myLeads)
-    for (let i=0; i<myLeads.length; i++){
-        console.log(myLeads[i])
-        ulEl.textContent = myLeads[i]
-    }
+    // console.log(myLeads)  
 })
+
+for (let i=0; i<myLeads.length; i++){
+    // console.log(myLeads[i])
+    ulEl.innerHTML +="<li>" + myLeads[i] + "</li>"
+}
 
 let tabBtn = document.querySelector('.tab-btn')
 tabBtn.addEventListener('click',function(){
