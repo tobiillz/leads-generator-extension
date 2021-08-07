fd2abf46e7bd31298162e6bee6ce6a9516eac638
Tobiillz <ayoadeopeoluwa@yahoo.com>
2021-08-07 10:46:08 +0100

    Save Button functional as expected

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
-let myLeads = ["opeoluwa","david","emmanuel"]
+let myLeads = []
 const inputEl = document.getElementById('input-el')
 const inputBtn = document.querySelector('.input-btn')
 const ulEl = document.getElementById('list-el')
 
+
 inputBtn.addEventListener('click',function(){
     myLeads.push(inputEl.value)
     // console.log(myLeads)  
+    renderLeads()
 })
 
-for (let i=0; i<myLeads.length; i++){
-    // console.log(myLeads[i])
-    ulEl.innerHTML +="<li>" + myLeads[i] + "</li>"
+
+function renderLeads(){
+    let listItems = ""
+    for (let i=0; i<myLeads.length; i++){
+        // console.log(myLeads[i])
+        listItems += "<li>" + myLeads[i] + "</li>"
+        //create list element
+        //const li = document.createElement('li')
+        //set text content 
+        //li.textContent = myLeads[i]
+        //append list element to ul
+        //ulEl.append(li)
+        
+    }ulEl.innerHTML = listItems  
 }
+
+
+
 
 let tabBtn = document.querySelector('.tab-btn')
 tabBtn.addEventListener('click',function(){
